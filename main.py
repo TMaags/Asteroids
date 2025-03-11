ff0745a699e70722c1d6d3c32c627fa483f9c266
TMaags <tjmaraglia94@gmail.com>
2025-03-11 15:00:52 -0400

    added shapes and players and basic rotation.

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
     pygame.init()
     print("Starting Asteroids!")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
+    #clock settings
+    clock = pygame.time.Clock()
+    dt= 0
+
+
 
     #game loop
     while True:
@@ -15,9 +23,15 @@ def main():
         
         #Fill the screen with black 
         screen.fill("black")
+        player.draw(screen)
+        player.update(dt)
 
         #update the display
         pygame.display.flip()
+
+        #time
+        dt = clock.tick(60)/1000    #time since last tick in seconds (limit game to 60fps)
+
 
 
     
